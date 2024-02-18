6e41d29a8bbbe040c6622159dcdfad461765b5a9
Mario <marioalexander998@gmail.com>
2024-02-18 16:20:37 +0700

    feat: migrate to tanstack react-query

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,45 @@
 'use client';
 
 // Node Modules
-import React, { createContext, useEffect, useMemo, useState } from "react";
+import React, { createContext, useMemo } from "react";
 import dynamic from "next/dynamic";
+import { useQuery, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Component
 import Image from "next/image";
 import InputIp from "@/components/InputIP";
-
-// Lib
-import useQuery from "@/lib/fetch";
 
 // Assets
 import backgroundImage from '../assets/pattern-bg-desktop.png';
 
 require('dotenv').config()
 
+const queryClient = new QueryClient();
+
 export const PositionContext = createContext({
   lat: -6.2,
   lng: 106.8
 });
 
+const fetchInitialGeolocation = async () => {
+  const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.NEXT_PUBLIC_GEOLOCATION_TOKEN}`);
+  const data = await response.json();
+
+  return data;
+};
+
 const IpAddressTrackerPage = () => {
   // Initial Fetch useQuery
-  const { data: initialGeoLocationData, isLoading } = useQuery('geolocation', `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.NEXT_PUBLIC_GEOLOCATION_TOKEN}`);
+  const { data: initialGeoLocationData, isLoading } = useQuery({
+    queryKey: ['get-geolocation'], 
+    queryFn: fetchInitialGeolocation,
+  }, queryClient);
   const {
     ip,
     isp: ispData = '',
     location: locationData,
-  } = initialGeoLocationData;
+  } = initialGeoLocationData || {};
+
   const {
     city = '',
     country = '',
@@ -47,57 +58,59 @@ const IpAddressTrackerPage = () => {
   ), []);
 
   return (
-    <main className="min-h-screen">
-      <div className={"absolute w-full pt-9 z-10"}>
-        <h1 className="text-center mb-9 text-white text-base font-bold">IP Tracker Address</h1>
-        {!isLoading && (
-          <InputIp initialIpAddress={ip}/>
-        )}
-        <div className={"rounded-2xl bg-white py-10 px-8 mt-12 mx-[12.5%] inline-block w-9/12"}>
-          <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
-            <div>
-              <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>IP ADDRESS</p>
-              {ip !== '' && (
-                <p className={"text-s font-semibold text-center dweb:text-left"}>{ip}</p>
-              )}
+    <QueryClientProvider client={queryClient}>
+      <main className="min-h-screen">
+        <div className={"absolute w-full pt-9 z-10"}>
+          <h1 className="text-center mb-9 text-white text-base font-bold">IP Tracker Address</h1>
+          {!isLoading && (
+            <InputIp initialIpAddress={ip}/>
+          )}
+          <div className={"rounded-2xl bg-white py-10 px-8 mt-12 mx-[12.5%] inline-block w-9/12"}>
+            <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
+              <div>
+                <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>IP ADDRESS</p>
+                {ip !== '' && (
+                  <p className={"text-s font-semibold text-center dweb:text-left"}>{ip}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
-            <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
-            <div className="mx-9">
-              <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>LOCATION</p>
-              {city !== '' && country !== '' && geonameId !== '' && (
-                <p className={"text-s font-semibold text-center dweb:text-left"}>{`${city} ${country} ${geonameId}`}</p>
-              )}
+            <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
+              <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
+              <div className="mx-9">
+                <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>LOCATION</p>
+                {city !== '' && country !== '' && geonameId !== '' && (
+                  <p className={"text-s font-semibold text-center dweb:text-left"}>{`${city} ${country} ${geonameId}`}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
-            <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
-            <div className="mx-9">
-              <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>TIMEZONE</p>
-              {timezoneData !== '' && (
-                <p className={"text-s font-semibold text-center dweb:text-left"}>{timezoneData}</p>
-              )}
+            <div className="mb-6 w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
+              <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
+              <div className="mx-9">
+                <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>TIMEZONE</p>
+                {timezoneData !== '' && (
+                  <p className={"text-s font-semibold text-center dweb:text-left"}>{timezoneData}</p>
+                )}
+              </div>
             </div>
-          </div>
-          <div className="w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
-            <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
-            <div className="mx-9">
-              <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>ISP</p>
-              {ispData !== '' && (
-                <p className={"text-s font-semibold text-center dweb:text-left"}>{ispData}</p>
-              )}
+            <div className="w-full dweb:inline-block dweb:align-top dweb:w-1/4 dweb:mb-0">
+              <div className={"hidden bg-gray-300 h-10 w-px absolute dweb:block"}/>
+              <div className="mx-9">
+                <p className={"text-xs text-gray-400 font-semibold text-center dweb:text-left"}>ISP</p>
+                {ispData !== '' && (
+                  <p className={"text-s font-semibold text-center dweb:text-left"}>{ispData}</p>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <Image className={"w-full h-72"} src={backgroundImage} alt={"background-image-ip-tracker"}/>
-      {!isLoading && (
-        <PositionContext.Provider value={{ lat: lat, lng: lng }}>
-          <Map />
-        </PositionContext.Provider>
-      )}
-    </main>
+        <Image className={"w-full h-72"} src={backgroundImage} alt={"background-image-ip-tracker"}/>
+        {!isLoading && (
+          <PositionContext.Provider value={{ lat: lat, lng: lng }}>
+            <Map />
+          </PositionContext.Provider>
+        )}
+      </main>
+    </QueryClientProvider>
   )
 };
 
